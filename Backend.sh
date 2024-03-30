4546eea49142bd5e184b1b8af242052f94e74e0c
Admin <dumbalakeerthana28@gmail.com>
2024-03-30 13:13:36 +0530

    Adding a step to remove Previous content

--- Backend.sh
+++ Backend.sh
@@ -2,13 +2,14 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd expense
+cp Backend.service /etc/systemd/system/backend.service
+rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
 unzip /tmp/backend.zip
 cd /app
 npm install
-cp Backend.service /etc/systemd/system/backend.service
-systemctl daemon-reload
+systemctl daemon-reloa
 systemctl enable backend
 systemctl start backend
