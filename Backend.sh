68757017acf92b9eae6f406d0439ef4e0ccd3fac
Admin <dumbalakeerthana28@gmail.com>
2024-03-30 14:17:54 +0530

    Created Expense.config file for frontend

--- Backend.sh
+++ Backend.sh
@@ -13,3 +13,6 @@ npm install
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
+
+dnf install mysql -y
+mysql -h 172.31.41.140 -uroot -pExpenseApp@1 < /app/schema/backend.sql
