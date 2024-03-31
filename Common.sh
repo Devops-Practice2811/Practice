50e8e1997c362c1da42d52118a25cb906da822a5
Admin <dumbalakeerthana28@gmail.com>
2024-03-31 21:49:27 +0530

    Added the check_status fun to display the exit code as success or failure instead of 0 or 1 or any other number

--- Common.sh
+++ Common.sh
@@ -3,9 +3,9 @@ Print_task_Heading(){
 }
 
 Check_Status(){
-  if [$1 -eq 0]; then
-    echo Sucess
-  else
-    echo Failure
-  fi
+  iif [ $1 -eq 0 ]; then
+   echo “Sucess""
+   else
+   echo “Failure”
+   fi
 }
